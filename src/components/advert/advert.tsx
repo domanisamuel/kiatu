207c68db2d6bbad1fb7b3ef5fac95581e45e1a46
domanisamuel <wafulasammie@gmail.com>
2020-12-05 19:20:11 +0300

    [feature]: advert and shortcuts

---
+++ src/components/advert/advert.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './advert.module.scss'
+
+const Advert: React.FC =()=> {
+    return (
+        <div className={styles.advert}>
+            <img src='https://i2.wp.com/nairobifashionhub.co.ke/wp-content/uploads/2019/03/Nairobi-Fashion-Hub-Vivo-Activewear-_3.jpg?resize=640%2C243&ssl=1' alt='' />
+        </div>
+    )
+}
+export default Advert
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,11 +3,15 @@ import styles from './Home.module.scss'
 
 // components
 import MainSlide from '../../components/main-slide/main-slide'
+import Advert from '../../components/advert/advert'
+import Shortcuts from '../../components/shortcuts/shortcuts'
 
 const HomePage: React.FC =()=> {
     return (
         <div className={styles.home}>
             <MainSlide/>
+            <Advert/>
+            <Shortcuts/>
         </div>
     )
 }
